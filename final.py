dba5cc9dad1fe59f497825b9c6ec70db6b5e0ca8
dellasa1 <81584772+dellasa1@users.noreply.github.com>
2021-03-29 13:49:32 -0600

    Update final.py

--- final.py
+++ final.py
@@ -1,16 +1,23 @@
 import random
-flip = input("Enter 'heads' or 'tails': ")
 
-coin = random.randint(1, 2)
-print(coin)
-
-if flip == 'heads':
-    if coin == 1:
-        print("That's correct!")
-    if coin == 2:
-        print("That's incorrect!")
-if flip == 'tails':
-    if coin == 1:
-        print("That's incorrect!")
-    if coin == 2:
-        print("That's correct!")
+score = 0
+playing = True
+while playing:
+    flip = input("Enter 'heads' or 'tails': ")
+    coin = random.randint(1, 2)
+    print(coin)
+    if flip == 'heads':
+        if coin == 1:
+            print("That's correct!")
+            score+=1
+        if coin == 2:
+            print("That's incorrect!")
+            playing = False
+    if flip == 'tails':
+        if coin == 1:
+            print("That's incorrect!")
+            playing = False
+        if coin == 2:
+            print("That's correct!")
+            score+=1
+print('Your score is ', score)
