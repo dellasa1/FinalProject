91ded0794f7d8638c7fa0ca685b025b98196c0b1
dellasa1 <81584772+dellasa1@users.noreply.github.com>
2021-03-29 13:43:01 -0600

    Update final.py

--- final.py
+++ final.py
@@ -1,3 +1,16 @@
-print('hello world')
-x = 10
-print(x)
+import random
+flip = input("Enter 'heads' or 'tails': ")
+
+coin = random.randint(1, 2)
+print(coin)
+
+if flip == 'heads':
+    if coin == 1:
+        print("That's correct!")
+    if coin == 2:
+        print("That's incorrect!")
+if flip == 'tails':
+    if coin == 1:
+        print("That's incorrect!")
+    if coin == 2:
+        print("That's correct!")
