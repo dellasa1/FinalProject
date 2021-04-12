d4364907507dba9ba02cb4b9c683d6a0ac0f33dc
dellasa1 <81584772+dellasa1@users.noreply.github.com>
2021-04-12 14:40:58 -0600

    Update final.py

--- final.py
+++ final.py
@@ -1,9 +1,31 @@
-#This class helps to store the list in the binary file with objects
+#defines objects in the leaderboard list
 class Leaders:
     def __init__(self, name, score, date):
         self.name = name
         self.score = score
         self.date = date                   
+#Defines objects in the player high score dictionary
+class PlayerScore:
+    def __init__(self, name, score, date):
+        self.name = name
+        self.score = score
+        self.date = date
+    def setName(self, name):
+        self.name = name
+    def setScore(self, score):
+        self.score = score
+    def setDate(self, date):
+        self.date = date
+    def getName(self):
+        return self.name
+    def getScore(self):
+        return self.score
+    def getDate(self):
+        return self.date
+    def __str__(self):
+        return 'Name: ' + self.name + \
+            '\nScore: ' + self.score + \
+                '\nDate: ' + self.date
 
 from datetime import datetime
 import random
@@ -11,56 +33,52 @@ import pickle
 
 play = 1
 leader = 2
-playerHigh = 3
+player = 3
 playerSpec = 4
 EXIT = 5
-DEL = 6
-
 #files to store information in
-topFile = 'leaderboard3.dat'
-playerFile = 'playerStat.dat'
+topFile = 'leaderboard4.dat'
+playerFile = 'playerStat4.dat'
 
 def main():
-    #retrieve lists from binary files
+    #gets lists from binary files
     leadList = getLeader()
-    #playHigh = getPlayerHigh()
+    playHigh = getPlayerHigh()
     
     choice = 0
     while choice != EXIT:
         choice = getChoice()
         if choice == play:
-            leadList = playGame(leadList)#, playHigh
+            leadList, playHigh = playGame(leadList, playHigh)#
         if choice == leader:
             showLeader(leadList)
-        if choice == playerHigh:
-            playerHigh()
+        if choice == player:
+            playerHigh(playHigh)
         if choice == playerSpec:
-            playerSpec()
-        if choice == DEL:
-            delete(leadList)
+            playerSpec(playHigh)
     saveLeaders(leadList)
-    #saveplayHigh(playHigh)
+    saveplayHigh(playHigh)
 
 def getLeader():
     try:
         infile = open(topFile, 'rb')
         leadList = pickle.load(infile)
-        print(leadList)
         infile.close()
     except:
         leadList = []
     return leadList
 
-#def getPlayerHigh():
-    #try:
-        #infile = open(playerFile, 'rb')
-        #playHigh = pickle.load(infile)
-        #infile.close()
-    #except:
-        #playHigh = []
-    #return playHigh
-
-#Gets user choice for what to make the program do
+def getPlayerHigh():
+    try:
+        infile = open(playerFile, 'rb')
+        playHigh = pickle.load(infile)
+        print(playHigh)
+        infile.close()
+    except:
+        playHigh = {}
+    return playHigh
+    
+#Gives user a choice for what to make the program do
 def getChoice():
     print()
     print('1. Play the game')
@@ -68,17 +86,15 @@ def getChoice():
     print('3. View all players high scores')
     print("4. Retrieve a specific player's high score")
     print('5. Exit the program')
-    #Temporary deletion of dictionary from file
-    print('6. delete dictionary from file')
     print()
     
     choice = int(input('Enter the number for an option above: '))
-    while choice < play or choice > DEL:
+    while choice < play or choice > EXIT:
         choice = int(input('Enter a valid option: '))
     return choice
-
-#Play the game
-def playGame(leadList):
+        
+#plays the game
+def playGame(leadList, playHigh):
     score = 0
     numObjects = 0
     playing = True
@@ -107,18 +123,28 @@ def playGame(leadList):
     print()
     date = str(now.strftime("%I:%M%p on %B %d, %Y"))
     score = str(score)
+#adds info and updates high score for each player
+    playClass = PlayerScore(name, score, date)
+    if name not in playHigh:
+        playHigh[name] = playClass
+        print('You have a high score')
+    else:
+        if score > playHigh[name].score:
+            #del playHigh[name]
+            playHigh[name] = playClass
+            print('You have a NEW high score')
 
     #Prints info and adds it to the list
     print('Name: ', name, '\nScore: ',
           score, '\nDate: ', date)
-    for x in leadList:
-        numObjects += 1
-    if numObjects < 5:
+    for x in leadList:#
+        numObjects += 1#
+    if numObjects < 5:#
         temp = Leaders(name, score, date)
         leadList.append(temp)
         print('You are one of the first people on the leaderboard')
 
-    #add it, order it, then remove the min v
+    #add it, order it, then remove min
     else:
         tempList = []
         for x in leadList:
@@ -133,8 +159,8 @@ def playGame(leadList):
         else:
             print("You didn't make the leaderboard")        
 
-    return leadList
-#Displays the leaderboard correctly from the list, specifying what place each score is
+    return leadList, playHigh
+#Displays the leaderboard
 def showLeader(leadList):
     leadList = sort(leadList)
     numbers = ['1st', '2nd', '3rd', '4th', '5th']
@@ -147,8 +173,12 @@ def showLeader(leadList):
 def sort(leadList):
     leadList = sorted(leadList, key=lambda x: x.score, reverse=True)
     return leadList
-#stores the high score for each player
-#def playerHigh():
+#Displays high score for each player
+def playerHigh(playHigh):
+    for x in playHigh:
+        print()
+        print(playHigh.get(x, 'Invalid'))
+        print()
 
 #Gets a specific player's high score
 #def playerSpec():
@@ -157,13 +187,9 @@ def saveLeaders(leadList):
     outFile = open(topFile, 'wb')
     pickle.dump(leadList, outFile)
     outFile.close()
-
-
-#def saveplayHigh(playHigh):
-    #outFile = open(playerFile, 'wb')
-    #pickle.dump(playHigh, outFile)
-    #outFile.close()
-def delete(leadList):
-    leadList.clear()
+#stores the list and dictionary in separate files
+def saveplayHigh(playHigh):
+    outFile = open(playerFile, 'wb')
+    pickle.dump(playHigh, outFile)
+    outFile.close()
 main()
-
